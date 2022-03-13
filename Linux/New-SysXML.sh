1a51daced461f522fb01e7a93507283c898322f5
Toby-Broom <toby@tobster.co.uk>
2022-03-13 18:33:42 +0100

    Added shell script for Linux

---
+++ Linux/New-SysXML.sh
@@ -0,0 +1,19 @@
+echo "<?xml version="1.0" encoding="UTF-8"?>" >SysInfo.xml
+echo "<!--" >>SysInfo.xml
+echo "System Infomation Report" >>SysInfo.xml
+echo "v1.3" >>SysInfo.xml
+echo " -->" >>SysInfo.xml
+echo "<SysInfo>" >>SysInfo.xml
+echo "  <Computer />" >>SysInfo.xml
+echo "  <Updated />" >>SysInfo.xml
+echo "  <Availability />" >>SysInfo.xml
+echo "  <Uptime_Form />" >>SysInfo.xml
+echo "  <CPUUse />" >>SysInfo.xml
+echo "  <CPUTemp />" >>SysInfo.xml
+echo "  <CPUPower />" >>SysInfo.xml
+echo "  <GPUUse />" >>SysInfo.xml
+echo "  <GPUTemp />" >>SysInfo.xml
+echo "  <GPUPower />" >>SysInfo.xml
+echo "  <DiskUse />" >>SysInfo.xml
+echo "  <MemoryUse />" >>SysInfo.xml
+echo "</SysInfo>">>SysInfo.xml
